651b6ae6e9d6624207dbb0b0402d7f85c6ed9caf
Drew Martin <drew.e.martin@gmail.com>
2014-06-24 21:15:09 -0400

    added form partials

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,63 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to products_path(@product)
+    else
+      render 'edit'
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product
+      @product.save
+      redirect_to products_path
+    else
+      render 'new'
+    end
+  end
+
+  def delete
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
